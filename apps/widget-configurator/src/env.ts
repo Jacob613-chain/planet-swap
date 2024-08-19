ad2f831c119c8fb7470d526ff18d72b564dab3b6
BestCryptoKnight <108866734+BestCryptoKnight@users.noreply.github.com>
2024-08-19 11:59:37 +0800

    fix: I updated

--- apps/widget-configurator/src/env.ts
+++ apps/widget-configurator/src/env.ts
@@ -1,5 +1,5 @@
-// export const isLocalHost = ['localhost', '127.0.0.1'].includes(window.location.hostname)
+export const isLocalHost = ['localhost', '127.0.0.1'].includes(window.location.hostname)
 
-// export const isVercel = window.location.hostname.includes('')
+export const isVercel = window.location.hostname.includes('')
 
-// export const isDev = ['dev.widget.cow.fi', 'dev.swap.cow.fi'].includes(window.location.hostname)
+export const isDev = ['dev.widget.cow.fi', 'dev.swap.cow.fi'].includes(window.location.hostname)
